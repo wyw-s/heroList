775c15c445f8761903374a7b8eba07b97e112a29
wyw-s <wyw.s@foxmail.com>
2020-08-02 12:26:48 +0800

    添加由于Mysql超时断开重连的方法

--- utils/mysql.js
+++ utils/mysql.js
@@ -1,21 +1,37 @@
 const mysql = require('mysql')
 
-// 创建链接
-var connection = mysql.createConnection({
+const sqlConfig = {
   host: 'localhost', // 数据库地址
   port: '3306',
   user: 'root',
   password: 'WANGyawei@520',
   database: 'heroList' // 数据库名称
-})
+}
 
-// 链接数据库
-connection.connect(function (err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack)
-    return
-  }
-  console.log('connection success')
-})
+function handleDisconnection() {
+  // 创建链接
+  var connection = mysql.createConnection(sqlConfig)
+  // 链接数据库
+  connection.connect(function (err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack)
+      setTimeout(handleDisconnection, 2000)
+    } else {
+      console.log('connection success')
+    }
+  })
 
-module.exports = connection
+  // 监听错误事件
+  connection.on('error', function (err) {
+    console.error('mysql error', err)
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.error('mysql error执行重连:' + err.message)
+      handleDisconnection()
+    } else {
+      throw err
+    }
+  })
+  return connection
+}
+
+module.exports = handleDisconnection()
